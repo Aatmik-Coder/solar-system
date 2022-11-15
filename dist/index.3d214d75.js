88ba1c864e122acf604be8d32e3a6549805c7fd5
Aatmik-Coder <coderaatmik@gmail.com>
2022-11-15 19:10:10 +0530

    node modules

--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -667,7 +667,7 @@ window.addEventListener("resize", function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-},{"three":"ktPTu","./img/stars.jpg":"fF64W","./img/sun.jpg":"8PtyY","./img/mercury.jpg":"hmDJ6","./img/venus.jpg":"jSsSo","./img/earth.jpg":"jaaP2","./img/mars.jpg":"iSzJw","./img/jupiter.jpg":"kqHAW","./img/saturn.jpg":"51I9j","./img/saturn ring.png":"2IZXy","./img/uranus.jpg":"eOyL6","./img/uranus ring.png":"6xW78","./img/neptune.jpg":"dsKOx","./img/pluto.jpg":"ki2Fb","@parcel/transformer-js/src/esmodule-helpers.js":"9YMAy","three/examples/jsm/controls/OrbitControls.js":"7mqRv","three/examples/jsm/webxr/ARButton.js":"7lQCi"}],"ktPTu":[function(require,module,exports) {
+},{"three":"ktPTu","./img/stars.jpg":"fF64W","./img/sun.jpg":"8PtyY","./img/mercury.jpg":"hmDJ6","./img/venus.jpg":"jSsSo","./img/earth.jpg":"jaaP2","./img/mars.jpg":"iSzJw","./img/jupiter.jpg":"kqHAW","./img/saturn.jpg":"51I9j","./img/saturn ring.png":"2IZXy","./img/uranus.jpg":"eOyL6","./img/uranus ring.png":"6xW78","./img/neptune.jpg":"dsKOx","./img/pluto.jpg":"ki2Fb","@parcel/transformer-js/src/esmodule-helpers.js":"9YMAy","three/examples/jsm/webxr/ARButton.js":"7lQCi","three/examples/jsm/controls/OrbitControls.js":"7mqRv"}],"ktPTu":[function(require,module,exports) {
 /**
  * @license
  * Copyright 2010-2022 Three.js Authors
@@ -29945,7 +29945,131 @@ module.exports = require("./helpers/bundle-url").getBundleURL("UckoE") + "neptun
 },{"./helpers/bundle-url":"i9rVb"}],"ki2Fb":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("UckoE") + "pluto.fbb933c2.jpg" + "?" + Date.now();
 
-},{"./helpers/bundle-url":"i9rVb"}],"7mqRv":[function(require,module,exports) {
+},{"./helpers/bundle-url":"i9rVb"}],"7lQCi":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "ARButton", ()=>ARButton);
+class ARButton {
+    static createButton(renderer, sessionInit = {}) {
+        const button = document.createElement("button");
+        function showStartAR() {
+            if (sessionInit.domOverlay === undefined) {
+                const overlay = document.createElement("div");
+                overlay.style.display = "none";
+                document.body.appendChild(overlay);
+                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                svg.setAttribute("width", 38);
+                svg.setAttribute("height", 38);
+                svg.style.position = "absolute";
+                svg.style.right = "20px";
+                svg.style.top = "20px";
+                svg.addEventListener("click", function() {
+                    currentSession.end();
+                });
+                overlay.appendChild(svg);
+                const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+                path.setAttribute("d", "M 12,12 L 28,28 M 28,12 12,28");
+                path.setAttribute("stroke", "#fff");
+                path.setAttribute("stroke-width", 2);
+                svg.appendChild(path);
+                if (sessionInit.optionalFeatures === undefined) sessionInit.optionalFeatures = [];
+                sessionInit.optionalFeatures.push("dom-overlay");
+                sessionInit.domOverlay = {
+                    root: overlay
+                };
+            }
+            //
+            let currentSession = null;
+            async function onSessionStarted(session) {
+                session.addEventListener("end", onSessionEnded);
+                renderer.xr.setReferenceSpaceType("local");
+                await renderer.xr.setSession(session);
+                button.textContent = "STOP AR";
+                sessionInit.domOverlay.root.style.display = "";
+                currentSession = session;
+            }
+            function onSessionEnded() {
+                currentSession.removeEventListener("end", onSessionEnded);
+                button.textContent = "START AR";
+                sessionInit.domOverlay.root.style.display = "none";
+                currentSession = null;
+            }
+            //
+            button.style.display = "";
+            button.style.cursor = "pointer";
+            button.style.left = "calc(50% - 50px)";
+            button.style.width = "100px";
+            button.textContent = "START AR";
+            button.onmouseenter = function() {
+                button.style.opacity = "1.0";
+            };
+            button.onmouseleave = function() {
+                button.style.opacity = "0.5";
+            };
+            button.onclick = function() {
+                if (currentSession === null) navigator.xr.requestSession("immersive-ar", sessionInit).then(onSessionStarted);
+                else currentSession.end();
+            };
+        }
+        function disableButton() {
+            button.style.display = "";
+            button.style.cursor = "auto";
+            button.style.left = "calc(50% - 75px)";
+            button.style.width = "150px";
+            button.onmouseenter = null;
+            button.onmouseleave = null;
+            button.onclick = null;
+        }
+        function showARNotSupported() {
+            disableButton();
+            button.textContent = "AR NOT SUPPORTED";
+        }
+        function showARNotAllowed(exception) {
+            disableButton();
+            console.warn("Exception when trying to call xr.isSessionSupported", exception);
+            button.textContent = "AR NOT ALLOWED";
+        }
+        function stylizeElement(element) {
+            element.style.position = "absolute";
+            element.style.bottom = "20px";
+            element.style.padding = "12px 6px";
+            element.style.border = "1px solid #fff";
+            element.style.borderRadius = "4px";
+            element.style.background = "rgba(0,0,0,0.1)";
+            element.style.color = "#fff";
+            element.style.font = "normal 13px sans-serif";
+            element.style.textAlign = "center";
+            element.style.opacity = "0.5";
+            element.style.outline = "none";
+            element.style.zIndex = "999";
+        }
+        if ("xr" in navigator) {
+            button.id = "ARButton";
+            button.style.display = "none";
+            stylizeElement(button);
+            navigator.xr.isSessionSupported("immersive-ar").then(function(supported) {
+                supported ? showStartAR() : showARNotSupported();
+            }).catch(showARNotAllowed);
+            return button;
+        } else {
+            const message = document.createElement("a");
+            if (window.isSecureContext === false) {
+                message.href = document.location.href.replace(/^http:/, "https:");
+                message.innerHTML = "WEBXR NEEDS HTTPS"; // TODO Improve message
+            } else {
+                message.href = "https://immersiveweb.dev/";
+                message.innerHTML = "WEBXR NOT AVAILABLE";
+            }
+            message.style.left = "calc(50% - 90px)";
+            message.style.width = "180px";
+            message.style.textDecoration = "none";
+            stylizeElement(message);
+            return message;
+        }
+    }
+}
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"9YMAy"}],"7mqRv":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "OrbitControls", ()=>OrbitControls);
@@ -30629,130 +30753,6 @@ class MapControls extends OrbitControls {
     }
 }
 
-},{"three":"ktPTu","@parcel/transformer-js/src/esmodule-helpers.js":"9YMAy"}],"7lQCi":[function(require,module,exports) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-parcelHelpers.export(exports, "ARButton", ()=>ARButton);
-class ARButton {
-    static createButton(renderer, sessionInit = {}) {
-        const button = document.createElement("button");
-        function showStartAR() {
-            if (sessionInit.domOverlay === undefined) {
-                const overlay = document.createElement("div");
-                overlay.style.display = "none";
-                document.body.appendChild(overlay);
-                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                svg.setAttribute("width", 38);
-                svg.setAttribute("height", 38);
-                svg.style.position = "absolute";
-                svg.style.right = "20px";
-                svg.style.top = "20px";
-                svg.addEventListener("click", function() {
-                    currentSession.end();
-                });
-                overlay.appendChild(svg);
-                const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-                path.setAttribute("d", "M 12,12 L 28,28 M 28,12 12,28");
-                path.setAttribute("stroke", "#fff");
-                path.setAttribute("stroke-width", 2);
-                svg.appendChild(path);
-                if (sessionInit.optionalFeatures === undefined) sessionInit.optionalFeatures = [];
-                sessionInit.optionalFeatures.push("dom-overlay");
-                sessionInit.domOverlay = {
-                    root: overlay
-                };
-            }
-            //
-            let currentSession = null;
-            async function onSessionStarted(session) {
-                session.addEventListener("end", onSessionEnded);
-                renderer.xr.setReferenceSpaceType("local");
-                await renderer.xr.setSession(session);
-                button.textContent = "STOP AR";
-                sessionInit.domOverlay.root.style.display = "";
-                currentSession = session;
-            }
-            function onSessionEnded() {
-                currentSession.removeEventListener("end", onSessionEnded);
-                button.textContent = "START AR";
-                sessionInit.domOverlay.root.style.display = "none";
-                currentSession = null;
-            }
-            //
-            button.style.display = "";
-            button.style.cursor = "pointer";
-            button.style.left = "calc(50% - 50px)";
-            button.style.width = "100px";
-            button.textContent = "START AR";
-            button.onmouseenter = function() {
-                button.style.opacity = "1.0";
-            };
-            button.onmouseleave = function() {
-                button.style.opacity = "0.5";
-            };
-            button.onclick = function() {
-                if (currentSession === null) navigator.xr.requestSession("immersive-ar", sessionInit).then(onSessionStarted);
-                else currentSession.end();
-            };
-        }
-        function disableButton() {
-            button.style.display = "";
-            button.style.cursor = "auto";
-            button.style.left = "calc(50% - 75px)";
-            button.style.width = "150px";
-            button.onmouseenter = null;
-            button.onmouseleave = null;
-            button.onclick = null;
-        }
-        function showARNotSupported() {
-            disableButton();
-            button.textContent = "AR NOT SUPPORTED";
-        }
-        function showARNotAllowed(exception) {
-            disableButton();
-            console.warn("Exception when trying to call xr.isSessionSupported", exception);
-            button.textContent = "AR NOT ALLOWED";
-        }
-        function stylizeElement(element) {
-            element.style.position = "absolute";
-            element.style.bottom = "20px";
-            element.style.padding = "12px 6px";
-            element.style.border = "1px solid #fff";
-            element.style.borderRadius = "4px";
-            element.style.background = "rgba(0,0,0,0.1)";
-            element.style.color = "#fff";
-            element.style.font = "normal 13px sans-serif";
-            element.style.textAlign = "center";
-            element.style.opacity = "0.5";
-            element.style.outline = "none";
-            element.style.zIndex = "999";
-        }
-        if ("xr" in navigator) {
-            button.id = "ARButton";
-            button.style.display = "none";
-            stylizeElement(button);
-            navigator.xr.isSessionSupported("immersive-ar").then(function(supported) {
-                supported ? showStartAR() : showARNotSupported();
-            }).catch(showARNotAllowed);
-            return button;
-        } else {
-            const message = document.createElement("a");
-            if (window.isSecureContext === false) {
-                message.href = document.location.href.replace(/^http:/, "https:");
-                message.innerHTML = "WEBXR NEEDS HTTPS"; // TODO Improve message
-            } else {
-                message.href = "https://immersiveweb.dev/";
-                message.innerHTML = "WEBXR NOT AVAILABLE";
-            }
-            message.style.left = "calc(50% - 90px)";
-            message.style.width = "180px";
-            message.style.textDecoration = "none";
-            stylizeElement(message);
-            return message;
-        }
-    }
-}
-
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"9YMAy"}]},["dqjOR","bB7Pu"], "bB7Pu", "parcelRequire94c2")
+},{"three":"ktPTu","@parcel/transformer-js/src/esmodule-helpers.js":"9YMAy"}]},["dqjOR","bB7Pu"], "bB7Pu", "parcelRequire94c2")
 
 //# sourceMappingURL=index.3d214d75.js.map
